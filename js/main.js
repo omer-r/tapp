8372e34138f5854e15109e392b60080fae3137a8
omer-r <omereemy@outlook.com>
2015-03-03 18:35:45 +0200

    part 3

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
 var app = {
 
-		findByName: function() {
-			console.log('findByName');
-			this.store.findByName($('.search-key').val(), function(employees) {
-				var l = employees.length;
-				var e;
-				$('.employee-list').empty();
-				for (var i=0; i<l; i++) {
-					e = employees[i];
-					$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-				}
-			});
-		},
-		
-		showAlert: function (message, title) {
-		    if (navigator.notification) {
-		        navigator.notification.alert(message, null, title, 'OK');
-		    } else {
-		        alert(title ? (title + ": " + message) : message);
-		    }
-		},
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
-		initialize: function() {
-			//this.store = new MemoryStore();
-			//this.store = new LocalStorageStore();
-			//this.store = new WebSqlStore();
-			this.store = new MemoryStore(function() {
-		        self.showAlert('Store Initialized', 'Info');
-		    });
-			$('.search-key').on('keyup', $.proxy(this.findByName, this));
-		}
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
 
 };
 
